ad50e300813c82166dbbecad75c1c4e93adec3d4
Aleksandr Cheverda <alexar@ua.fm>
2022-11-22 23:19:09 +0200

    upd files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import { Container } from './App.Styled';
@@ -10,114 +10,79 @@ import Button from "components/Button";
 import Modal from "components/Modal";
 import Loader from "components/Loader";
 
-class App extends Component {
-  state = {
-    inputValue: '',
-    images: [],
-    page: 1,
-    isLoading: false,
-    showModal: null,
-    error: null,
-  };
-  componentDidUpdate(_, prevState) {
-    const prevSearch = prevState.inputValue;
-    const currentSearch = this.state.inputValue;
-    const prevPage = prevState.page;
-    const currentPage = this.state.page;
-
-    if (prevSearch !== currentSearch || prevPage !== currentPage) {
-      this.updImages();
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(null);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    if (inputValue === '') {
+      return;
     }
-  }
-  async updImages() {
-    const { inputValue, page } = this.state;
-    this.setState({ isLoading: true });
-    try {
-      await fetchImages(inputValue, page).then(res => {
-        if (!res.data.hits.length) {
-          return toast.error(
-            'There is no images with this request, please, try again'
-          );
-        }
-        const newImg = mapPictures(res.data.hits);
-        this.setState({
-          images: [...this.state.images, ...newImg],
+    const updateImages = () => {
+      setIsLoading(true);
+      try {
+        fetchImages(inputValue, page).then(data => {
+          if (!data.hits.length) {
+            return toast.error(
+              'There is no images with this request, please, try again'
+            );
+          }
+          const newImg = mapPictures(data.hits);
+          setImages(images => [...images, ...newImg]);
         });
-      });
       } catch (error) {
-        this.setState({ error });
+        setError(error);
       } finally {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
       }
-    }
-    handleSearchSubmit = inputValue => {
-      this.setState({
-        inputValue,
-        images: [],
-        page: 1,
-      });
     };
-
-    loadMore = () => {
-      this.setState(prevState => ({
-        page: prevState.page + 1,
-      }));
-    };
-
-    showModalImage = largeImageURL => {
-      const image = this.state.images.find(
-        image => image.largeImageURL === largeImageURL
-      );
-      this.setState({
-        showModal: {
-          largeImageURL: image.largeImageURL,
-          tags: image.tags,
-        },
-      });
-    };
-  
-    closeModalImage = () => {
-      this.setState({ showModal: null });
-    };
-    
-    render() {
-      const { images, isLoading, error, showModal } = this.state;
-      const { handleSearchSubmit, showModalImage, loadMore, closeModalImage } =
-        this;
-      return (
-        <Container>
-          <Searchbar onSearch={handleSearchSubmit} />
-          {error &&
-            toast.warning(`Ooops, something went wrong: ${error.message}`)}
-          {isLoading && <Loader />}
-          {images.length > 0 && (
-            <>
-              <ImageGallery images={images} openModal={showModalImage} />
-              <Button loadMore={loadMore} />
-            </>
-          )}
-          {showModal && (
-            <Modal
-              lgImg={showModal.largeImageURL}
-              tags={showModal.tags}
-              closeModal={closeModalImage}
-            />
-          )}
-
-          <ToastContainer
-            position="top-right"
-            autoClose={4000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-          />
-        </Container>
-      );
-    }
-  }
+    updateImages();
+  }, [inputValue, page]);
+  const showModalImage = largeImageURL => {
+    const image = images.find(image => image.largeImageURL === largeImageURL);
+    setShowModal({
+      largeImageURL: image.largeImageURL,
+      tags: image.tags,
+    });
+  };
+  const closeModalImage = () => {
+    setShowModal(null);
+  };
+  return (
+    <Container>
+      <Searchbar onSearch={handleSearchSubmit} />
+      {error &&
+        toast.warning(`Ooops, something went wrong: ${error.message}`)}
+      {isLoading && <Loader />}
+      {images.length > 0 && (
+        <>
+          <ImageGallery images={images} openModal={showModalImage} />
+          <Button loadMore={loadMore} />
+        </>
+      )}
+      {showModal && (
+        <Modal
+          lgImg={showModal.largeImageURL}
+          tags={showModal.tags}
+          closeModal={closeModalImage}
+        />
+      )}
+      <ToastContainer
+        position="top-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </Container>
+  );
+};
 
 export default App;
